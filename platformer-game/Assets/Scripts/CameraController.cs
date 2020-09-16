aeb8018e870caf6a899ff155b0a997209a60f528
FixxxarN <tornstromandre@gmail.com>
2020-09-16 21:58:31 +0200

    Added a player and a tilemap

---
+++ platformer-game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CameraController : MonoBehaviour
+    {
+        private GameObject player;
+
+        void Start()
+        {
+            player = FindObjectOfType<PlayerController>().gameObject;
+        }
+
+        void Update()
+        {
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+        }
+    }
+}
---
+++ platformer-game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed;
+    private Rigidbody2D rigidBody;
+    
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            rigidBody.velocity = new Vector2(-movementSpeed, rigidBody.velocity.y);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            rigidBody.velocity = new Vector2(movementSpeed, rigidBody.velocity.y);
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+        }
+    }
+}
